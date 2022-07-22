48876ceda53aa3a4276cf9db53deabd5ebfded39
Matheus Consul <mattheusbc@gmail.com>
2022-07-22 00:07:01 -0300

    21.07

--- servidor.c
+++ servidor.c
@@ -10,6 +10,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -33,7 +36,7 @@ void iniciarServidor (){
     struct sockaddr_in saddr = {
         .sin_family      = AF_INET,
         .sin_addr.s_addr = htonl(INADDR_ANY),
-        .sin_port        = htons(5000)
+        .sin_port        = htons(4000)
     };
 
 
@@ -42,7 +45,7 @@ void iniciarServidor (){
     int csize  = sizeof caddr;
 
     bind(server, (struct sockaddr *) &saddr, sizeof saddr);
-    listen(server, 5); //especifica o n´umero de conex˜oes que podem estar em espera num socket de um servidor (por exemplo, chamadas telef´onicas em espera)
+    listen(server, 5); //especifica o numero de conexoes que podem estar em espera num socket de um servidor (por exemplo, chamadas telefonicas em espera)
 
 
     while(1){
@@ -57,10 +60,10 @@ void iniciarServidor (){
         pthread_create(&thredClientes[numClientesOnline],NULL, novoCliente,&client);
        
         fflush(stdout);
-                 
+        
         // PROBLEMA AO FECHAR O NAVEGAR SEM FECHAR A CONEXÃO ENCERRA A EXECUÇÃO DO SERVIDOR 
         
-        puts("\n >>>>> Fim do while e zerando o conteudo de client \n");
+        puts("\n >>>>> Fim do while de escuta \n");
        // memset(&client, 0, sizeof client);  
     }
 
@@ -104,15 +107,45 @@ void responderCliente (int cliente){
 
     printf("\n >>>>> Thread do cliente %d DENTRO DO responderCliente \n",cliente);
 
-    char resposta[500] = "HTTP/1.0 200 OK\nDate: Tue, 09 Aug 2011 15:44:04 GMT\nServer: Apache/2.2.3(CentOS)\nLast-Modified: Tue, 09 Aug 2020 15:11:03 GMT\nContent-Length: 6821\nContent-Type: text/html\nConnection: Closed\n\n<html><body><h1>Esta e a resposta do servidor cliente nº: !!!<h1><body></html>";
 
-    send(cliente,resposta,500, 0); // endereço socket cliente, char de resposta, tamanho da resposta e 0 nao lembro    
+    FILE *arquivo;
+
+    char caminho[100] = "/home/ec/Documentos/GitHub/EC-Servidor-HTTP/imagem001.jpeg";
+
+
+    if ((arquivo = open(caminho, O_RDONLY)) > 0){
+
+            puts("Image Found.");
+            int bytes;
+            char buffer[1024];
+
+            send(cliente, "HTTP/1.0 200 OK\r\nContent-Type: image/jpeg\r\n\r\n", 45, 0); 
+                bytes = read(arquivo,buffer,1024);
+            write(cliente,buffer,bytes);
+
+	        while ( (bytes=read(arquivo, buffer, 1024))>0 ) {// lendo o arquivo do buffer
+                    printf("\nDENTRO DO WHILE DO ENVIO DE ARQUIVO");
+			      write (cliente, buffer, bytes); // enviando jpeg para cliente
+              }
+
+             
+
+    }
+
+
+    char resposta1[500] = "HTTP/1.1 200 OK\nDate: Tue, 09 Aug 2011 15:44:04 GMT\nServer: Apache/2.2.3(CentOS)\nLast-Modified: Tue, 09 Aug 2020 15:11:03 GMT\nContent-Length: 6821\nContent-Type: text/html\nConnection: Closed\n\n<html><body><h1>Esta e a resposta do servidor cliente nº: !!! <img src=\"http://www.lkmescritorios.com.br/wp-content/uploads/2016/04/5-D%C3%9AVIDAS-ESCRIT%C3%93RIO-VIRTUAL-720x720.jpg\"> <h1><body></html>";
+
+    char resposta2[500] = "HTTP/1.1 200 OK\nDate: Tue, 09 Aug 2011 15:44:04 GMT\nServer: Apache/2.2.3(CentOS)\nLast-Modified: Tue, 09 Aug 2020 15:11:03 GMT\nContent-Length: 6821\nContent-Type: text/html\nConnection: Closed\n\n<html><body><h1>Esta e a resposta do servidor cliente nº: !!! <img src=\"/home/ec/Documentos/GitHub/EC-Servidor-HTTP/imagem001.jpg\"> <h1><body></html>";
+
+    char resposta3[500] = "HTTP/1.1 200 OK\nDate: Tue, 09 Aug 2011 15:44:04 GMT\nServer: Apache/2.2.3(CentOS)\nLast-Modified: Tue, 09 Aug 2020 15:11:03 GMT\nContent-Length: 6821\nContent-Type: text/html\nConnection: Closed\n\n<html><body><h1>Esta e a resposta do servidor cliente nº: !!! <h1><body></html>";
+
+    //send(cliente,resposta1,500, 0); // endereço socket cliente, char de resposta, tamanho da resposta e 0 nao lembro    
 
 }
 
 
 
-int lerRequisicao(char requisicao[], int cliente ){
+int lerRequisicao(char *requisicao, int cliente ){
 
     char metado[10] = "\0";
     char protocolo[10] = "\0";
@@ -172,9 +205,13 @@ int lerRequisicao(char requisicao[], int cliente ){
     metado[2]= '\0';  // apenas para teste no terminal 
     metado[3]= '\0';
     printf("========================\n");
-    printf("\n metado: %s \n",metado);
+    printf("metado: %s \n",metado);
     printf("protocolo: %s",protocolo);
     printf("versao: %s",versao);
+    printf("========================\n");
+    printf("========================\n");
+    puts(requisicao);
+    printf("========================\n");
     printf("========================\n");
 
     
@@ -187,6 +224,7 @@ int lerRequisicao(char requisicao[], int cliente ){
     }else {
         printf("\n >>>>> nao identificou comando de sair x = 1");
         x=1;
+        memset(&requisicao, 0, sizeof requisicao);
         responderCliente(cliente);
     }
 
