e68bee0a2ff8270a275be792aaddf8be5d126292
Matheus Consul <mattheusbc@gmail.com>
2022-07-22 00:44:10 -0300

    Ajuste de versão

--- 1ª Versão/main.c
+++ 1ª Versão/main.c
@@ -7,16 +7,57 @@
 */
 
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
-#include "servidor.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 
 
 
 int main(){
 
+    struct sockaddr_in caddr;
+    struct sockaddr_in saddr = {
+        .sin_family      = AF_INET,
+        .sin_addr.s_addr = htonl(INADDR_ANY),
+        .sin_port        = htons(4000)
+    };
 
-    iniciarServidor ();
 
-    return 0;
+    int server = socket(AF_INET, SOCK_STREAM, 0);
+    int client;
+    int csize  = sizeof caddr;
+
+     char buff[500];
+
+    bind(server, (struct sockaddr *) &saddr, sizeof saddr);
+    listen(server, 5); //especifica o numero de conexoes que podem estar em espera num socket de um servidor (por exemplo, chamadas telefonicas em espera)
+
+
+    while(1){
+        puts("\n >>>>> Inicio do while de escuta da porta \n");
+
+        client = accept(server, (struct sockaddr *) &caddr, &csize);
+
+        recv(client, buff, sizeof buff, 0); // tinha ponteiro *cliente //Realiza a leitura do buffer de entrada do cliente
+        printf("\n >>>>> Cliente %d LEU O BUFFER \n",client);
+       
+        fflush(stdout);
+
+        char resposta[50] = "\n\n Resposta do servior \n Hello word!\n\n";
+
+        send(client,resposta,50, 0); // endereço socket cliente, char de resposta, tamanho da resposta e 0 nao lembro  
+
+        close(client); // fechando a conexão com o cliente 
+
+        memset(&buff, 0, sizeof buff);
+        
+        puts("\n >>>>> Fim do while de escuta \n");
+
+    }
+
 }
-
